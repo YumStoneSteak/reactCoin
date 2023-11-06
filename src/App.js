a61e04911108dfe6e0008a52957ce40eaa97f9f3
Donghyuk Lee <leedonghyuk2163@gmail.com>
2023-11-06 12:26:59 +0900

    코인 계산기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [userMoney, setUserMoney] = useState("1");
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const calMoney = (coin) => userMoney / coin;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>coins ({loading ? null : coins.length})</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <>
+          <input
+            type="number"
+            placeholder="how much do you have in"
+            onChange={(e) => setUserMoney(e.target.value)}
+            value={userMoney}
+          ></input>{" "}
+          USD
+          <br />
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol} : {calMoney(coin.quotes.USD.price)}{" "}
+                USD)
+              </option>
+            ))}
+          </select>
+        </>
+      )}
     </div>
   );
 }
